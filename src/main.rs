4433a6b375f47573c71bacf38a9f8c0804c67c19
Egor Minyazev <minyazev.egor050@gmail.com>
2026-09-22 00:39:56 +0300

    Test #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut path = String::new();
+
+    io::stdin().read_line(&mut path).expect("Error");
+
+    let path = path.trim();
+
+    if fs::read_to_string(path).is_ok() {
+        println!("success");
+    } else {
+        println!("failure");
+    }
 }
