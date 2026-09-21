01f392d6ecdf0ce4fb558bb12833fd52d0e203c2
Egor Minyazev <minyazev.egor050@gmail.com>
2026-09-22 00:43:53 +0300

    Test #2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::fs;
+use std::fs::File;
 use std::io;
 
 fn main() {
     let mut path = String::new();
-
     io::stdin().read_line(&mut path).expect("Error");
-
     let path = path.trim();
 
-    if fs::read_to_string(path).is_ok() {
+    if File::open(path).is_ok() {
         println!("success");
     } else {
         println!("failure");
