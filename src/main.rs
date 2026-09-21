76109e3a2ced59a48f07e09651abadeaa56312a0
Egor Minyazev <minyazev.egor050@gmail.com>
2026-09-22 01:38:18 +0300

    check is dir, 1 byte read

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 use std::fs::File;
-use std::io;
+use std::io::{self, Read};
+use std::path::Path;
 
 fn main() {
-    let mut path = String::new();
-    io::stdin().read_line(&mut path).expect("Error");
-    let path = path.trim();
+    let mut file_path = String::new();
 
-    if File::open(path).is_ok() {
-        println!("success");
-    } else {
+    if io::stdin().read_line(&mut file_path).is_err() {
+        println("failure");
+        return;
+    }
+
+    let trimmed_path = file_path.trim();
+    if trimmed_path.is_empty() {
         println!("failure");
+        return;
+    }
+
+    let p = Path::new(trimmed_path);
+    if p.is_dir() {
+        println!("failure");
+        return;
+    }
+    let mut file = match File::open(trimmed_path) {
+        Ok(f) => f,
+        Err(_) => {
+            println!("failure");
+            return;
+        }
+    };
+    let mut buffer = [0u8; 1];
+    match file.read(&mut buffer) {
+        Ok(_) => println!("success"),
+        Err(_) => println!("failure"),
     }
 }
